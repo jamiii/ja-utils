2e81d9c7e3781e5f63fda90c5d0ceade13b84019
James M <jmiloser@gmail.com>
2022-08-27 15:06:01 -0400

    fix conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'd
+epub_show_urls = 'footnote'
 
 # -- Options for Theme
 html_theme_options = {
